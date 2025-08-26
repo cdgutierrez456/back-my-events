c70ccdced4fe097e39d5f8a32dfe0a3ca442d253
Cristian Gutierrez <cdgutierrez456@gmail.com>
2025-08-26 10:33:28 -0500

    Habilitando creacion de tablas

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.routes.auth import router as auth_router
 from app.routes.user import router as user_router
 from app.routes.event import router as event_router
 from app.routes.profile import router as profile_router
-# from .database import create_tables
+from .database import create_tables
 import app.models
 
 app = FastAPI()
@@ -28,9 +28,9 @@ app.include_router(user_router, prefix='/api/users', tags=['Users'])
 app.include_router(event_router, prefix='/api/events', tags=['Events'])
 app.include_router(profile_router, prefix='/api/profiles', tags=['Profiles'])
 
-# @app.on_event("startup")
-# async def on_startup():
-#     await create_tables()
+@app.on_event("startup")
+async def on_startup():
+    await create_tables()
 
 @app.get("/")
 def read_root():
